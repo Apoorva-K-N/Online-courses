eb62928564f18e1c7de880266086aec10c9f7c56
Apoorva-K-N <44522212+Apoorva-K-N@users.noreply.github.com>
2020-05-22 20:34:53 +0530

    Add files via upload

---
+++ Day 5/coding/Program 1(Day 5).py
@@ -0,0 +1,30 @@
+#Python program to print all Prime #numbers in an Interval
+#Given two positive integer start and end. #The task is to write a Python program #toprint all Prime numbers in an Interval.
+
+
+start = int(input("Enter the first number\n"))
+end = int(input("Enter the last number\n"))
+print("The prime numbers in the given range are : \n")
+for i in range(start,end+1):
+  if (i>1):
+    for j in range(2,i):
+      if(i%j==0):
+        break
+    else:
+        print(i,"\n")
+  
+
+Output:
+Enter the first number
+5
+Enter the last number
+15
+The prime numbers in the given range are : 
+
+5 
+
+7 
+
+11 
+
+13 
